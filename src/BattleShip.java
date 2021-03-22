b22387ed3ebc3b4a460994f347d7c1efa0ea1186
Irina Ryabova <irina2013266@gmail.com>
2021-03-22 08:20:36 +0300

    Добавлен класс Ship

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class BattleShip {
+public class BattleShip implements Play {
+    @Override
+    public void addShip(Ship ship) {
+
+    }
+
+    @Override
+    public void shotFirst() {
+
+    }
+
+    @Override
+    public boolean shot(int x, int y) {
+        return false;
+    }
+
+    @Override
+    public boolean wounded() {
+        return false;
+    }
+
+    @Override
+    public boolean doNotWounded() {
+        return false;
+    }
+
+    @Override
+    public void isWin() {
+
+    }
 }
--- src/Ship.java
+++ src/Ship.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Ship {
+public class Ship {
+    private int[][] field = new int[10][10];
+    private int[][] firstShips;
+    private int[][] secondShips;
+
+    public Ship(int[][] field) {
+        this.field = field;
+    }
+
+    public int[][] getFirstShips() {
+        return firstShips;
+    }
+
+    public void setFirstShips(int[][] firstShips) {
+        this.firstShips = firstShips;
+    }
+
+    public int[][] getSecondShips() {
+        return secondShips;
+    }
+
+    public void setSecondShips(int[][] secondShips) {
+        this.secondShips = secondShips;
+    }
 }
